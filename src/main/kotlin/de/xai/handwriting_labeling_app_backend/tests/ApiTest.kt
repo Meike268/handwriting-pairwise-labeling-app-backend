d3e2d240f0acb8b1a0b093702d0ce8dae9f9de8b
Lukas <lukas.pieger@prontoweb.de>
2024-07-19 14:10:11 +0200

    feat: Add some dummy-api-endpoints

---
+++ src/main/kotlin/de/xai/handwriting_labeling_app_backend/tests/ApiTest.kt
@@ -0,0 +1,24 @@
+package de.xai.handwriting_labeling_app_backend.tests
+
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class ApiTest {
+
+    @GetMapping("/api/ping")
+    fun ping(): ResponseEntity<out String> {
+        return ResponseEntity.ok("pong")
+    }
+
+    @PostMapping("/api/addOne")
+    fun addOne(@RequestBody number: Int): ResponseEntity<out Int> {
+        return ResponseEntity.ok(number + 1)
+    }
+
+    @PutMapping("/api/greeting", consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun greeting(@RequestBody greeting: Greeting): ResponseEntity<String> {
+        return ResponseEntity.ok("${greeting.clause} ${greeting.name}")
+    }
+}
---
+++ src/main/kotlin/de/xai/handwriting_labeling_app_backend/tests/Greeting.kt
@@ -0,0 +1,6 @@
+package de.xai.handwriting_labeling_app_backend.tests
+
+data class Greeting(
+    val clause: String,
+    val name: String,
+)
