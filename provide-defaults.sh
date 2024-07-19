07c1380b4a94e3e8189686d6ba6f4e2c8a944b9f
Lukas <lukas.pieger@prontoweb.de>
2024-07-19 20:05:34 +0200

    feat: Add a mysql-connection (hopefully :D)

---
+++ provide-defaults.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR" || exit 1
+
+ENV_FILE=.env
+if [ ! -f "$ENV_FILE" ]; then
+  echo \
+"MYSQL_HOST=localhost
+MYSQL_PORT=3306
+MYSQL_DATABASE=handwriting_labeling_app
+MYSQL_USERNAME=xxxxxxxxxxxxxxx
+MYSQL_PASSWORD=xxxxxxxxxxxxxxx
+" \
+> "$ENV_FILE"
+fi
