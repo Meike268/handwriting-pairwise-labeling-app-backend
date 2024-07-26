31bb41759a0598b009b73bb0064f5d964e1804a3
Lukas <lukas.pieger@prontoweb.de>
2024-07-26 17:30:04 +0200

    feat: Couldn't help but to implement a couple of not yet needed convenience-methods😬

--- src/main/kotlin/de/xai/handwriting_labeling_app_backend/model/Sample.kt
+++ src/main/kotlin/de/xai/handwriting_labeling_app_backend/model/Sample.kt
@@ -1,7 +1,17 @@
 package de.xai.handwriting_labeling_app_backend.model
 
+import de.xai.handwriting_labeling_app_backend.repository.SampleRepository
+import java.io.File
+
 data class Sample(
     val id: Long,
     val studentId: Long,
     val referenceSentence: ReferenceSentence?
-)
+) {
+    fun getResouceFile(): File {
+        return SampleRepository.getResourceFile(this)
+    }
+    fun getResourceUrl(): String {
+        return SampleRepository.getResourceUrl(this)
+    }
+}
--- src/main/kotlin/de/xai/handwriting_labeling_app_backend/repository/SampleRepository.kt
+++ src/main/kotlin/de/xai/handwriting_labeling_app_backend/repository/SampleRepository.kt
@@ -4,15 +4,17 @@ import de.xai.handwriting_labeling_app_backend.model.Sample
 import org.springframework.stereotype.Repository
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import java.io.File
+import java.io.FileNotFoundException
 
 @Repository
 class SampleRepository(
     private val referenceSentenceRepository: ReferenceSentenceRepository
 ) {
-    private final val samplesDir = File("src/main/resources/public/files/images/samples")
-    private final val xaiSentencesDir = File(samplesDir.path + "/xai_sentences")
-
     fun fromFile(file: File): Sample {
+        if (!file.exists()) {
+            throw FileNotFoundException()
+        }
+
         val studentId = file.name.split("_")[0].toLong()
         val sampleId = file.name.split("_")[1].replace(".png", "").toLong()
         return if (file.parentFile.parentFile == xaiSentencesDir)
@@ -30,16 +32,48 @@ class SampleRepository(
         }.toList()
     }
 
+    fun findById(id: Long): Sample? {
+        return findAll().find { it.id == id }
+    }
+
+    /**
+     * Returns a sample if the corresponding File exists or null if it doesn't.
+     */
+    fun findByIdAndStudentIdAndReferenceSentenceId(id: Long, studentId: Long, referenceSentenceId: Long?): Sample? {
+        return try {
+            fromFile(getResourceFile(id, studentId, referenceSentenceId))
+        } catch (e: FileNotFoundException) {
+            null
+        }
+    }
+
     companion object {
-        fun getResourceUrl(sample: Sample): String {
+        private val samplesDir = File("src/main/resources/public/files/images/samples")
+        private val xaiSentencesDir = File(samplesDir.path + "/xai_sentences")
+
+        fun getResourceUrl(id: Long, studentId: Long, referenceSentenceId: Long?): String {
             val baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString()
             val sampleUrl = "$baseUrl/files/images/samples"
 
-            return if (sample.referenceSentence != null) {
-                "$sampleUrl/xai_sentences/${sample.referenceSentence.id}/${sample.studentId}_${sample.id}.png"
+            return if (referenceSentenceId != null) {
+                "$sampleUrl/xai_sentences/${referenceSentenceId}/${studentId}_${id}.png"
             } else {
-                "$sampleUrl/greenline_de/${sample.studentId}_${sample.id}.png"
+                "$sampleUrl/others/${studentId}_${id}.png"
             }
+        }
+        fun getResourceUrl(sample: Sample): String {
+            return getResourceUrl(sample.id, sample.studentId, sample.referenceSentence?.id)
+        }
+
+        fun getResourceFile(id: Long, studentId: Long, referenceSentenceId: Long?): File {
+            return File(if (referenceSentenceId != null) {
+                "$xaiSentencesDir/${referenceSentenceId}/${studentId}_${id}.png"
+            } else {
+                "$samplesDir/others/${studentId}_${id}.png"
+            })
+        }
+        fun getResourceFile(sample: Sample): File {
+            return getResourceFile(sample.id, sample.studentId, sample.referenceSentence?.id)
         }
     }
 }
