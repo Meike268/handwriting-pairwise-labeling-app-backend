369d935d21a0a1e2694e5612afc10b468f2d1ea7
erikjonathanschmidt <erik.jonathan.schmidt@stabilo.com>
2024-08-09 15:30:36 +0200

    - only load samples once instead of loading all samples once per answer

--- src/main/kotlin/de/xai/handwriting_labeling_app_backend/service/AnswerService.kt
+++ src/main/kotlin/de/xai/handwriting_labeling_app_backend/service/AnswerService.kt
@@ -7,6 +7,7 @@ import de.xai.handwriting_labeling_app_backend.utils.Constants.Companion.XAI_SEN
 import de.xai.handwriting_labeling_app_backend.utils.Constants.Companion.othersDirectory
 import de.xai.handwriting_labeling_app_backend.utils.Constants.Companion.xaiSentencesDirectory
 import de.xai.handwriting_labeling_app_backend.utils.safeLet
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
@@ -18,7 +19,15 @@ class AnswerService(
     private val referenceSentenceRepository: ReferenceSentenceRepository,
     private val sampleRepository: SampleRepository,
 ) {
-    fun createOrUpdate(username: String, sampleId: Long, questionId: Long, score: Int, submissionTimestamp: LocalDateTime): Answer {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun createOrUpdate(
+        username: String,
+        sampleId: Long,
+        questionId: Long,
+        score: Int,
+        submissionTimestamp: LocalDateTime
+    ): Answer {
         return answerRepository.save(
             Answer(
                 user = userRepository.findByUsername(username)!!,
@@ -34,6 +43,8 @@ class AnswerService(
         includeXaiSamplesInExport: Boolean,
         includeOthersSamplesInExport: Boolean
     ): ExportAnswersBody {
+        logger.info("Prepare export of answers. includeXAi = $includeXaiSamplesInExport, includeOthers = $includeOthersSamplesInExport")
+        val startTime = System.currentTimeMillis()
         var xaiMetaData: XAiExportAnswersMetaData? = null
         var xaiAnswerInfos: List<XAiExportAnswerInfoBody>? = null
         var othersAnswerInfos: List<OthersExportAnswersBody>? = null
@@ -47,6 +58,7 @@ class AnswerService(
             othersAnswerInfos = retrieveOthersAnswers()
         }
 
+        logger.info("Creating the answer export body took ${System.currentTimeMillis() - startTime} ms")
         return ExportAnswersBody(
             xaiMetaData = xaiMetaData,
             xaiAnswers = xaiAnswerInfos,
@@ -55,13 +67,16 @@ class AnswerService(
     }
 
 
-    private fun retrieveXaiAnswers() = answerRepository.findAll().mapNotNull { answer ->
-        // search sample of this answer in samples/xai_sentences and read ref sent id from it
-        val referenceSentenceId = answer.sampleId?.let { sampleId ->
-            sampleRepository.findAllInDirectoryRecursive(xaiSentencesDirectory).find { sample ->
-                sample.id == sampleId
-            }?.referenceSentence?.id
-        } ?: return@mapNotNull null //the sample this answer refers to is not in xai directory
+    private fun retrieveXaiAnswers(): List<XAiExportAnswerInfoBody> {
+        val allXaiSamples = sampleRepository.findAllInDirectoryRecursive(xaiSentencesDirectory)
+
+        return answerRepository.findAll().mapNotNull { answer ->
+            // search sample of this answer in samples/xai_sentences and read ref sent id from it
+            val referenceSentenceId = answer.sampleId?.let { sampleId ->
+                allXaiSamples.find { sample ->
+                    sample.id == sampleId
+                }?.referenceSentence?.id
+            } ?: return@mapNotNull null //the sample this answer refers to is not in xai directory
 
             safeLet(
                 answer.user?.id,
@@ -80,9 +95,12 @@ class AnswerService(
                     submissionTimestamp = time.toString()
                 )
             }
-                ?: throw IllegalStateException("Problem constructing ${XAiExportAnswerInfoBody::class.java.name} from " +
-                        "answers in DB.\n Answer: $answer")
+                ?: throw IllegalStateException(
+                    "Problem constructing ${XAiExportAnswerInfoBody::class.java.name} from " +
+                            "answers in DB.\n Answer: $answer"
+                )
         }
+    }
 
     private fun retrieveXaiMetaData(
         xaiAnswerInfos: List<XAiExportAnswerInfoBody>
@@ -100,29 +118,33 @@ class AnswerService(
         )
     }
 
-    fun retrieveOthersAnswers() = answerRepository.findAll().mapNotNull { answer ->
-        // search sample of this answer in samples/others
-        answer.sampleId?.let { sampleId ->
-            sampleRepository.findAllInDirectoryRecursive(othersDirectory).find { sample ->
-                sample.id == sampleId
+    fun retrieveOthersAnswers(): List<OthersExportAnswersBody> {
+        val allOthersSamples = sampleRepository.findAllInDirectoryRecursive(othersDirectory)
+
+        return answerRepository.findAll().mapNotNull { answer ->
+            // search sample of this answer in samples/others
+            answer.sampleId?.let { sampleId ->
+                allOthersSamples.find { sample ->
+                    sample.id == sampleId
+                }
+            } ?: return@mapNotNull null //the sample this answer refers to is not in others directory
+
+            safeLet(
+                answer.user?.id,
+                answer.sampleId,
+                answer.score
+            ) { userId, sampleId, score ->
+
+                OthersExportAnswersBody(
+                    userId = userId,
+                    sampleId = sampleId,
+                    score = score
+                )
             }
-        } ?: return@mapNotNull null //the sample this answer refers to is not in others directory
-
-        safeLet(
-            answer.user?.id,
-            answer.sampleId,
-            answer.score
-        ) { userId, sampleId, score ->
-
-            OthersExportAnswersBody(
-                userId = userId,
-                sampleId = sampleId,
-                score = score
-            )
+                ?: throw IllegalStateException(
+                    "Problem constructing ${OthersExportAnswersBody::class.java.name} from " +
+                            "answers in DB.\n Answer: $answer"
+                )
         }
-            ?: throw IllegalStateException(
-                "Problem constructing ${OthersExportAnswersBody::class.java.name} from " +
-                        "answers in DB.\n Answer: $answer"
-            )
     }
 }
