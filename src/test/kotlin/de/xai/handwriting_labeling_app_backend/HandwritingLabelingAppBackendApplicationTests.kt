36a2b899781a6e4362837d5ef9ef01903e411b22
erikjonathanschmidt <erik.jonathan.schmidt@stabilo.com>
2024-08-16 11:40:19 +0200

    - removed not working test

--- src/test/kotlin/de/xai/handwriting_labeling_app_backend/HandwritingLabelingAppBackendApplicationTests.kt
+++ src/test/kotlin/de/xai/handwriting_labeling_app_backend/HandwritingLabelingAppBackendApplicationTests.kt
@@ -1,37 +1,13 @@
 package de.xai.handwriting_labeling_app_backend
 
-import de.xai.handwriting_labeling_app_backend.repository.AnswerRepository
-import de.xai.handwriting_labeling_app_backend.repository.SampleRepository
-import de.xai.handwriting_labeling_app_backend.repository.UserRepository
-import de.xai.handwriting_labeling_app_backend.service.AnswerService
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import java.time.LocalDateTime
 
 @SpringBootTest
-class HandwritingLabelingAppBackendApplicationTests(
-	val answerService: AnswerService,
-	val sampleRepository: SampleRepository
-) {
+class HandwritingLabelingAppBackendApplicationTests {
 
 	@Test
 	fun contextLoads() {
-	}
-
-	@Test
-	fun fillAnswersTable() {
-
-		val allSamples = sampleRepository.findAll()
-		for (sample in allSamples) {
-
-			answerService.createOrUpdate(
-				"user",
-				sampleId = sample.id,
-				questionId = 1,
-				score = 3,
-				submissionTimestamp = LocalDateTime.now()
-			)
-		}
 	}
 
 }
