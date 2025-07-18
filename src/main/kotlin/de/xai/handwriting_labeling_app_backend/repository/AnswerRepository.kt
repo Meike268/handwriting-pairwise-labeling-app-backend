a4dc4727c73e421c8405c5fc30908ea83626a3ad
Meike Valentina Bauer <meike-valentina.bauer@stud.uni-bamberg.de>
2025-07-18 11:26:42 +0200

    extend answer repository for comparisonList API

--- src/main/kotlin/de/xai/handwriting_labeling_app_backend/repository/AnswerRepository.kt
+++ src/main/kotlin/de/xai/handwriting_labeling_app_backend/repository/AnswerRepository.kt
@@ -3,6 +3,10 @@ package de.xai.handwriting_labeling_app_backend.repository
 import de.xai.handwriting_labeling_app_backend.model.Answer
 import org.springframework.data.jpa.repository.JpaRepository
 
+import jakarta.transaction.Transactional
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+
 interface AnswerRepository: JpaRepository<Answer, Long> {
     fun findAllByQuestionId(questionId: Long): List<Answer>
 
@@ -11,6 +15,15 @@ interface AnswerRepository: JpaRepository<Answer, Long> {
     fun findAllBySampleId1(sampleId1: Long): List<Answer>
 
     fun findAllBySampleId2(sampleId2: Long): List<Answer>
+
+    @Query("""
+        SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END
+        FROM Answer a
+        WHERE (a.sampleId1 = :sample1Id AND a.sampleId2 = :sample2Id)
+           OR (a.sampleId1 = :sample2Id AND a.sampleId2 = :sample1Id)
+    """)
+    fun existsBySamplePair(sample1Id: Long, sample2Id: Long): Boolean
+
 
 }
 
