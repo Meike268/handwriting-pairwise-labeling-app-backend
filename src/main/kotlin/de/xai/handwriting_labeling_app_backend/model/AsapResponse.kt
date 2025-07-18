86ca4648702af3853a739ce57a289d0370116ff1
Meike Valentina Bauer <meike-valentina.bauer@stud.uni-bamberg.de>
2025-07-18 15:50:30 +0200

    remove unused files

--- src/main/kotlin/de/xai/handwriting_labeling_app_backend/model/AsapResponse.kt
+++
@@ -1,7 +0,0 @@
-package de.xai.handwriting_labeling_app_backend.model
-
-
-data class AsapResponse(
-    val pairs: List<List<Int>>,
-    val mean_eig: Double
-)
--- src/main/kotlin/de/xai/handwriting_labeling_app_backend/repository/UserBatchLogRepository.kt
+++
@@ -1,9 +0,0 @@
-package de.xai.handwriting_labeling_app_backend.repository
-
-import de.xai.handwriting_labeling_app_backend.model.User
-import org.springframework.data.jpa.repository.JpaRepository
-import de.xai.handwriting_labeling_app_backend.model.UserBatchLog
-
-interface UserBatchLogRepository : JpaRepository<UserBatchLog, Long> {
-    fun countByUserId(userId: Long): Int
-}
